714b7439becbe8cb45a37638f3de84c7d33e3005
Hsankesara <heetsankesara3@gmail.com>
2018-10-07 18:19:52 +0530

    Added gitignore

--- team28-Automation/iscreening/api/views.py
+++ team28-Automation/iscreening/api/views.py
@@ -9,12 +9,12 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 from django.contrib.auth import authenticate
 
 config = {
-    apiKey: "AIzaSyCOpasPOo620XMm0-JCVnllJSwJ4v4WVkE",
-    authDomain: "int-screening.firebaseapp.com",
-    databaseURL: "https://int-screening.firebaseio.com",
-    projectId: "int-screening",
-    storageBucket: "int-screening.appspot.com",
-    messagingSenderId: "691103875926"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 
 firebase = pyrebase.initialize_app(config)
